09b385c7b58b5c3f0187fcdcc6a2c2bf35dcf215
jugbeep <patrickbotkins@gmail.com>
2018-01-31 15:12:40 -0700

    lets see what happens

--- back-end/server.js
+++ back-end/server.js
@@ -1,15 +1,37 @@
+require('dotenv').config();
+
+const express = require('express');
+const app = express();
+const path = require('path');
+
+if(!process.env.DYNO) {
+    app.use(function(req, res, next) {
+      res.header("Access-Control-Allow-Origin", "http://localhost:4200");
+  res.header("Access-Control-Allow-Credentials", "true");
+      res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+      res.header("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE");
+      next();
+    });
+  }
+
+app.use(express.static(__dirname + '/dist'));
+
+  app.get('/*', function(req, res) {
+    res.sendFile(path.join(__dirname + '/dist/index.html'));
+  });
+
 /************************************************
 /			Put your name in this let 			*
 ************************************************/
 let name = 'sassankermani';
 
 //set up for app
-let express = require('express');
-let app = express();
+//let express = require('express');
+//let app = express();
 //let bodyParser = ('body-parser');
 let port = process.env.PORT || 3000;
 //let Sequelize = require('sequelize');
-//let sequelize = new Sequelize('postgres://' + name + '@localhost:5432/dnddatabase');
+let sequelize = new Sequelize('postgres://' + name + '@localhost:5432/dnddatabase');
 
 //app using stuff
 app.use(bodyParser());
@@ -24,5 +46,5 @@ app.get('/', function(req, res){
 
 //listener
 app.listen(port, function(){
-	console.log('ap is up at ' + port);
+	console.log(`Listening on port ${port}`);
 });
