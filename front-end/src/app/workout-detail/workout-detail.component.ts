b78bcf411c6cb0d8af19923f9c2a0c58c01db787
jugbeep <patrickbotkins@gmail.com>
2018-02-03 15:49:11 -0700

    buttons save and revert

--- front-end/src/app/workout-detail/workout-detail.component.ts
+++ front-end/src/app/workout-detail/workout-detail.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnChanges, Input } from '@angular/core';
 //import { Workout } from '../workouts/workout'
 import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { Workout } from '../data-model';
@@ -9,12 +9,11 @@ import { WorkoutsService } from '../workouts.service';
   templateUrl: './workout-detail.component.html',
   styleUrls: ['./workout-detail.component.scss']
 })
-export class WorkoutDetailComponent {
+export class WorkoutDetailComponent implements OnChanges{
 
   @Input() workout: Workout;
 
   workoutForm: FormGroup;
-
 
 
   constructor(
@@ -32,14 +31,21 @@ export class WorkoutDetailComponent {
   	});
   }
 
+  ngOnChanges() {
+  	this.workoutForm.reset({
+  		name: this.workout.name
+  	});
+  }
+
   onSubmit() {
   	this.workout = this.prepareSaveWorkout();
   	this.workoutService.updateWorkout(this.workout).subscribe();
   	this.ngOnChanges()  
   }
   
-  prepareSaveWorkout() {
+  prepareSaveWorkout(): Workout {
   	const formModel = this.workoutForm.value;
+  	console.log(this.workoutForm.value)
   	const saveWorkout: Workout = {
   	  id: this.workout.id,
   	  name: formModel.name as string
@@ -47,10 +53,5 @@ export class WorkoutDetailComponent {
   	return saveWorkout;
   }
 
-  ngOnChanges() {
-  	this.workoutForm.reset({
-  		name: this.workout.name
-  	})
-  }
 };
  
--- front-end/src/app/workouts.service.ts
+++ front-end/src/app/workouts.service.ts
@@ -1,17 +1,23 @@
 import { Injectable } from '@angular/core';
-import { Workout } from './workouts/workout'
-import { WORKOUTS } from './workouts/mock-workouts';
 import { Observable } from 'rxjs/observable';
 import { of } from 'rxjs/observable/of';
+import { Workout, workouts } from './data-model';
 
 @Injectable()
 export class WorkoutsService {
 
   
 
-  constructor() { }
+  constructor(
+  ) { }
   
   getWorkouts(): Observable<Workout[]> {
-  	return of(WORKOUTS);
+  	return of(workouts);
+  }
+
+  updateWorkout(workout: Workout): Observable <Workout> {
+  	const oldWorkout = workouts.find(w => w.id === workout.id);
+  	const newWorkout = Object.assign(oldWorkout, workout);
+  	  return of(newWorkout);
   }
 }
