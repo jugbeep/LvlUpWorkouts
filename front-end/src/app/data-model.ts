ae79f9908c6ea2a0b9cbf99582c4a5f3ae2046c8
jugbeep <patrickbotkins@gmail.com>
2018-02-03 23:40:12 -0700

    issed something

---
+++ front-end/src/app/data-model.ts
@@ -0,0 +1,19 @@
+export class Workout {
+	id = 0;
+	name = '';
+}
+
+export const workouts: Workout[] = [
+  {
+  	id: 1,
+  	name: 'Situps',
+  },
+  {
+  	id: 2,
+  	name: 'Squats',
+  },
+  {
+  	id: 3,
+  	name: 'Burpees',
+  },
+];
