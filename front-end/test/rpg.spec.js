ab792c509d0a2a86da634c123a2664de8427cdbf
trumpetcoder <kevindjames55@gmail.com>
2018-02-06 18:28:53 -0700

    rendered monster api results

--- front-end/test/rpg.spec.js
+++ front-end/test/rpg.spec.js
@@ -1,11 +1,11 @@
 var expect = require('chai').expect;
 var request = require('request');
-var baseUrl = 'http://dnd5eapi.co/api';
+var api = 'http://dnd5eapi.co/api';
 
 // Front End Testing to hit dnd API endpoint monsters checked and working
 describe('returns monsters', function() {
 	it('returns monsters', function(done) {
-		request.get({ url: baseUrl + '/monsters/' },
+		request.get({ url: api+ '/monsters/' },
 			function(error, response, body) {
 				expect(response.statusCode).to.equal(200);
 				console.log(body);
@@ -17,7 +17,7 @@ describe('returns monsters', function() {
 // Front End Testing to hit dnd API endpoint monsters/3 dragon index checked and working
 describe('returns a monster at index 3', function() {
 	it('returns monsters at index 3', function(done) {
-		request.get({ url: baseUrl + '/monsters/3' },
+		request.get({ url: api + '/monsters/10' },
 			function(error, response, body) {
 				expect(response.statusCode).to.equal(200);
 				console.log(body);
