41787fa224013569b52721976b9d0a564556a785
trumpetcoder <kevindjames55@gmail.com>
2018-02-07 15:37:37 -0700

    changes

--- front-end/src/app/auth/signup/signup.component.ts
+++ front-end/src/app/auth/signup/signup.component.ts
@@ -26,21 +26,10 @@ export class SignupComponent implements OnInit {
   		this.authService.emailSignup(
   			newUser.value.email,
   			newUser.value.password
-<<<<<<< HEAD
+
   		)
   	}
     this.dbService.userSignup(newUser)
   }
 }
-=======
-  			);
-  		}
-      this.dbService.userSignup(newUser)
-  }
-}
 
-
-
-
-
->>>>>>> 7ed949add14303b9059eb7b7941b7aad3ee75b84
