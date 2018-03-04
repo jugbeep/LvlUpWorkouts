71e3e7f89948a5d6f0d9e22924bd74cee8721462
jugbeep <patrickbotkins@gmail.com>
2018-03-04 14:09:57 -0700

    merge issues?

--- back-end/models/index.js
+++ back-end/models/index.js
@@ -5,7 +5,7 @@ var Sequelize = require('sequelize');
 /	Put your name in this let	*
 ********************************/
 
-let name = 'sassankermani';
+let name = 'patrickbotkins';
 
 var sequelize = new Sequelize(process.env.DATABASE_URL || 'postgres://' + name + '@localhost:5432/dnddatabase');
 
